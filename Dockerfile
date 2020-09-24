c52df3bd12104fcce2b821babd8f387fcac4e13f
Victor Farfan <victor.fm@alliedglobalbpo.com>
2020-09-23 20:20:22 -0600

    base app

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# base image
+FROM node:10
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package*.json ./
+
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "npm", "run", "serve"]
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home!!!</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+UIkit.use(Icons);
+export default {
+  name: "App",
+};
+</script>>
+<style lang="less">
+@import "../node_modules/uikit/src/less/uikit.less";
+</style>
